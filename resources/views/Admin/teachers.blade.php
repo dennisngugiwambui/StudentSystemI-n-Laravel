5c85c0f6dba152e1ee1762299ada5a2a157882ef
Denno <dellyit001@gmail.com>
2024-05-16 03:10:23 -0700

    adding more files

--- resources/views/Admin/teachers.blade.php
+++ resources/views/Admin/teachers.blade.php
@@ -12,7 +12,7 @@
                     <div class="bg-secondary rounded d-flex align-items-center justify-content-between p-4">
                         <i class="fa fa-chart-line fa-3x text-primary"> </i>
                         <div class="ms-3">
-                            <p class="mb-2">Registered</p>
+                            <p class="mb-2">Registered Students</p>
                             <h6 class="mb-0">2932</h6>
                         </div>
                     </div>

754d23b52edba350723ee6b877e4b175d35f78bc
Denno <dellyit001@gmail.com>
2024-05-18 23:54:25 -0700

    teacher model and migration

--- database/migrations/2024_05_17_093343_create_employees_table.php
+++ database/migrations/2024_05_17_093343_create_employees_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('employees', function (Blueprint $table) {
             $table->id();
-
             $table->string('full_name');
             $table->date('date_of_birth');
             $table->string('gender');

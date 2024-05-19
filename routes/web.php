01e5fbaee87a262fdcbcaee45a2b96496d679cb0
Denno <dellyit001@gmail.com>
2024-05-19 03:23:15 -0700

    route changes

--- routes/web.php
+++ routes/web.php
@@ -75,5 +75,11 @@ Route::get('/new_Employees', [App\Http\Controllers\HomeController::class, 'new_E
 
 Route::post('/RegisterTeacher', [App\Http\Controllers\MainController::class, 'RegisterTeacher'])->name('RegisterTeacher');
 
+Route::get('/Communication', [\App\Http\Controllers\HomeController::class, 'Communication'])->name('Communication');
+
+Route::get('/Management', [\App\Http\Controllers\HomeController::class, 'Management'])->name('Management');
+
+
+
 ;
 

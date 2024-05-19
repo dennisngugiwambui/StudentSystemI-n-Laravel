bb21789e97b101956ec4d99289c9e7bbbcf159bf
Denno <dellyit001@gmail.com>
2024-05-19 03:18:15 -0700

    employee view

--- resources/views/Admin/navbar.blade.php
+++ resources/views/Admin/navbar.blade.php
@@ -5,10 +5,10 @@
     <a href="{{ route('student') }}" class="nav-item nav-link"><i class="fa fa-keyboard me-2"></i>Student</a>
     <a href="{{ route('teachers') }}" class="nav-item nav-link"><i class="fa fa-table me-2"></i>Teachers</a>
     <a href="{{route(' Employees')}}" class="nav-item nav-link"><i class="fa fa-chart-bar me-2"></i>Employees</a>
-    <a href="chart.html" class="nav-item nav-link"><i class="fa fa-chart-bar me-2"></i>Examinations</a>
-    <a href="chart.html" class="nav-item nav-link"><i class="fa fa-chart-bar me-2"></i>Attendances</a>
-    <a href="chart.html" class="nav-item nav-link"><i class="fa fa-chart-bar me-2"></i>Communication</a>
-    <a href="chart.html" class="nav-item nav-link"><i class="fa fa-chart-bar me-2"></i>Management</a>
+    <a href="{{route('Examination')}}" class="nav-item nav-link"><i class="fa fa-chart-bar me-2"></i>Examinations</a>
+    <a href="{{route('Attedance')}}" class="nav-item nav-link"><i class="fa fa-chart-bar me-2"></i>Attendances</a>
+    <a href="{{route('Communication')}}" class="nav-item nav-link"><i class="fa fa-chart-bar me-2"></i>Communication</a>
+    <a href="{{route('Management')}}" class="nav-item nav-link"><i class="fa fa-chart-bar me-2"></i>Management</a>
     <a href="{{ route('login_logs') }}" class="nav-item nav-link"><i class="fa fa-chart-bar me-2"></i>Logs</a>
     <a href="{{ route('Welfare') }}" class="nav-item nav-link"><i class="fa fa-chart-bar me-2"></i>Welfare</a>
 </div>

e796e0b0e46ce0322adac9a265cf37cb3f016f2a
Denno <dellyit001@gmail.com>
2024-08-13 00:21:32 -0700

    Leaveout changes

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -156,7 +156,6 @@ class HomeController extends Controller
         return view('Admin.Leaveout', compact('students'));
     }
 
-
     public function leadership()
     {
         $data= Leadership::all();

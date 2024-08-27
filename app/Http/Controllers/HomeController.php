6282723e000b996d2b2a3e561bf6ab3497b1e0a9
Denno <dellyit001@gmail.com>
2024-08-26 21:14:26 -0700

    Leaveout changes

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -201,6 +201,7 @@ class HomeController extends Controller
         return view('Admin.newCommunication');
     }
 
+
     public function EmployeeDetails($unique_id)
     {
        // $student = StudentRegister::with('skills')->where('unique_id', $unique_id)->first();

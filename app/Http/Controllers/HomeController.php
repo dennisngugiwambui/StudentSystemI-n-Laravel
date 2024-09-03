3cb13225c88a9ff2ddac2a47f4fc1f823e0dc803
Denno <dellyit001@gmail.com>
2024-09-02 21:07:50 -0700

    Leaveout changes

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -207,6 +207,7 @@ class HomeController extends Controller
         // $student = StudentRegister::with('skills')->where('unique_id', $unique_id)->first();
         $employee = Employee::where('unique_id', $unique_id)->first();
         return view('Admin.EmployeeDetails', compact('employee'));
+
 }
 
 

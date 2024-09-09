acd3bd9c29009e5fb75f2864184a84697086f574
Denno <dellyit001@gmail.com>
2024-09-09 01:10:26 -0700

    Leaveout changes

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -202,7 +202,7 @@ class HomeController extends Controller
         return view('Admin.newCommunication');
     }
 
-    public login 
+    public func login
 
     public function EmployeeDetails($unique_id)
     {

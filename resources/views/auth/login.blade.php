27abe63931175706d162067b18adc29f3d2df3e3
Denno <dellyit001@gmail.com>
2024-09-22 15:50:52 -0700

    Leaveout changes

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -19,7 +19,7 @@
         .login-box {
             background-color: white;
             padding: 20px;
-            border-radius: 10px
+            border-radius: 10p
             box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
             width: 350px;
         }

93eb12c68f97662953ed3eb704174fc0bcbdc5f2
Denno <dellyit001@gmail.com>
2024-10-11 22:52:14 -0700

    Leaveout changes

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -14,7 +14,7 @@
             justify-content: center;
             align-items: center;
             height: 100vh;
-            margin:
+            margin: 0
         }
         .login-box {
             background-color: white;
